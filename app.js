6db4b20eeadd8030a0107a72a78da69ab73aa2e9
LibbyRiley <libbyriley@windowslive.com>
2015-06-12 20:21:10 +1000

    Converted more standard html with directives
    Also change style.css to custom.css, and move it.

--- app.js
+++ app.js
@@ -5,27 +5,43 @@
         this.products = wands;
     });
 
-    app.controller('TabController', function(){
-        this.tab = 1;
 
-        this.setTab = function(newValue){
-          this.tab = newValue;
+    app.directive("productTabs", function() {
+        return {
+          restrict: "E",
+          templateUrl: "product-tabs.html",
+          controller: function() {
+            this.tab = 1;
+
+            this.isSet = function(checkTab) {
+              return this.tab === checkTab;
+            };
+
+            this.setTab = function(activeTab) {
+              this.tab = activeTab;
+            };
+          },
+          controllerAs: "tab"
         };
+      });
 
-        this.isSet = function(tabName){
-          return this.tab === tabName;
+    app.directive("productReviews", function() {
+        return {
+            restrict: "E",
+            templateUrl: "product-reviews.html",
+            controller: function() {
+                this.review = {};
+
+                this.addReview = function(product){
+                    this.review.createdOn = Date.now();
+                    product.reviews.push(this.review);
+                    this.review = {};
+                };
+            },
+            controllerAs: "review"
         };
     });
 
-    app.controller('ReviewController', function(){
-    	this.review = {};
-
-    	this.addReview = function(product){
-            this.review.createdOn = Date.now();
-    		product.reviews.push(this.review);
-    		this.review = {};
-    	};
-    });
 
     app.directive('woodDescription', function(){
         return {
@@ -43,7 +59,7 @@
 
     app.directive('productSpecs', function(){
         return {
-            restrict: 'A',
+            restrict: 'E',
             templateUrl: 'product-specs.html'
         };
     });
